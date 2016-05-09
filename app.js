6397a6ee1fbd6823b0d4a11164a1afa87d6ea582
Daniel Martínez <danieldt08@gmail.com>
2016-05-08 23:06:51 -0300

    New configurations

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Portafolio!');
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+  response.send('Portafolio!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function () {
+  console.log('App listening on port !', app.get('port'));
 });
